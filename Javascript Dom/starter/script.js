82eb04f1351b97c360aa126d2b474e53412b8872
Zakyboss <Boochimo8@gmail.com>
2024-10-08 15:07:15 +0300

    game progress

--- Javascript Dom/starter/script.js
+++ Javascript Dom/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+// console.log(document.querySelector(".message").textContent)
+// document.querySelector('.message').textContent = 'You lucky son of a Gun 😂'
+// console.log(document.getElementsByClassName('message')[0].textContent);
+// document.querySelector('.number').textContent= 13
+ let secretNumber = Math.trunc(Math.random()*20)+1
+ let score = 20 ;
+console.log(secretNumber)
+ let x = function (){
+    let numberInQuestion = document.querySelector('.number')
+    numberInQuestion.innerHTML=secretNumber
+    let guess = document.querySelector('.guess').value ;
+    guess = Number(guess) ;
+    console.log(guess, typeof guess )
+    if(!guess){
+        document.querySelector('.message').textContent = `You did not fill anything 🤐`
+    }
+    else if (guess < secretNumber){
+        document.querySelector('.message').textContent = `Too low 📉`
+        score= score -1 
+
+        document.querySelector('.score').textContent =score
+
+
+    } else if (guess > secretNumber){
+        document.querySelector('.message').textContent = `Too high📈`
+        score= score -1 
+        document.querySelector('.score').textContent =score
+
+
+    }
+} 
+document.querySelector('.check').addEventListener('click',x )
