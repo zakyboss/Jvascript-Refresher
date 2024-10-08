cc658c4edad84a69bf733da17f2738dd10950bf5
Zakyboss <Boochimo8@gmail.com>
2024-10-08 21:10:17 +0300

    Progress so far

--- Javascript Dom/starter/script.js
+++ Javascript Dom/starter/script.js
@@ -7,26 +7,49 @@
  let score = 20 ;
 console.log(secretNumber)
  let x = function (){
-    let numberInQuestion = document.querySelector('.number')
-    numberInQuestion.innerHTML=secretNumber
+
     let guess = document.querySelector('.guess').value ;
     guess = Number(guess) ;
     console.log(guess, typeof guess )
     if(!guess){
         document.querySelector('.message').textContent = `You did not fill anything 🤐`
+    } else if (guess===secretNumber){
+        document.querySelector('.number').innerHTML=secretNumber ;
+        document.querySelector('.number').style.width ='30rem'
+    
+        document.querySelector('.message').textContent = `You won  congratulations 🎉`
+        document.querySelector('body').style.backgroundColor ='green'
+ 
     }
     else if (guess < secretNumber){
-        document.querySelector('.message').textContent = `Too low 📉`
-        score= score -1 
+        if (score>1){
+            document.querySelector('.message').textContent = `Too low 📉`
+            score= score -1 
+    
+            document.querySelector('.score').textContent =score
+    
+        } else {
+            document.querySelector('.message').textContent = `You lost 😪`
+            score=0
+            document.querySelector('.score').textContent =score
 
-        document.querySelector('.score').textContent =score
-
+        }
+       
 
     } else if (guess > secretNumber){
-        document.querySelector('.message').textContent = `Too high📈`
-        score= score -1 
-        document.querySelector('.score').textContent =score
+       
+        if (score>1){
+            document.querySelector('.message').textContent = `Too high📈`
+            score= score -1 
+    
+            document.querySelector('.score').textContent =score
+    
+        } else {
+            document.querySelector('.message').textContent = `You lost 😪`
+            score=0
+            document.querySelector('.score').textContent =score
 
+        }
 
     }
 } 
