11e175a13f1136a43643225856c8d19c3c7d69d7
Zakyboss <Boochimo8@gmail.com>
2024-10-22 19:24:38 +0300

    progress

--- Javascript Dom/08-Behind-the-Scenes/starter/script.js
+++ Javascript Dom/08-Behind-the-Scenes/starter/script.js
@@ -5,6 +5,17 @@ function calcAge(birthYear){
     function printAge() {
         const output = `You are ${age},born in ${birthYear}`
       console.log(output)
+   if (birthYear>=1981 && birthYear<=1996){
+    var millennial = true
+        const str = `oh! you are a millenial , ${firstName}`
+//    console.log(str)
+function add (a,b){
+    return a+b
+
+}
+    }
+    // console.log(str)
+    console.log(millennial)
     }
     printAge();
    return age
