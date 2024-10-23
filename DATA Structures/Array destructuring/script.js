ae158b76806c5f8eb694285edffd1d2ed81452d5
Zakyboss <Boochimo8@gmail.com>
2024-10-23 17:45:12 +0300

    Progress

--- DATA Structures/Array destructuring/script.js
+++ DATA Structures/Array destructuring/script.js
@@ -7,6 +7,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order : function(starterIndex , mainIndex){
+    return [this.starterMenu[starterIndex] , this.mainMenu[mainIndex]
+ ] }
 };
 const arr = [1,2,3];
 // assuming that we wanted to copy each element of the array to a variable we would 
@@ -18,3 +21,33 @@ console.log(a,b,c)
 const [x,y,z]=arr ;
 console.log(x,y,z)
 // The original array is not destroyed
+
+// working with data now
+// taking elements from categories 
+let [first1 , second]= restaurant.categories;
+console.log(first1,second)
+
+    //  if we wanted to take 1st element of categories and 4th 
+    let [first2 , , ,fourth]= restaurant.categories;
+    // console.log(first1,fourth)
+
+    // assuming that we wanted to switch the third and second 
+    let [category1,,category3]=restaurant.categories;
+//     // let temp = category1;
+//     // category1= category3;
+//     // category3 = temp ;
+//     console.log(category1,category3)
+// [category1,category3]=[category3,category1];
+// // console.log(first)
+// console.log(category3,category1)
+// console.log(category1,category3)
+
+
+
+// let array = [1,2,4];
+// let [j,k,l]= array ;
+// console.log(j,k,l);
+// [j,k,l]= [l,k,j];
+// console.log(j,k,l)
+
+console.log(restaurant.order(1,2))
